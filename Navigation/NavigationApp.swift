cb4e3211d7557a1b7c76afab80231813987cbd1e
JoanMay132 <jcmd270@gmail.com>
2024-12-03 23:54:56 -0600

    Navigation project

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Joan May on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
